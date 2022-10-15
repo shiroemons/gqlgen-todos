60dcff0f6e2f710978512e686bfdba01c3619c58
shiroemons <shiroemons@gmail.com>
2022-10-15 15:28:45 +0900

    Add chi and cors

--- server.go
+++ server.go
@@ -8,8 +8,10 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/rs/cors"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 
@@ -20,6 +22,30 @@ import (
 const defaultPort = "8080"
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	db := newDB()
+	r := chi.NewRouter()
+
+	r.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func newDB() *bun.DB {
 	config, err := pgx.ParseConfig(os.Getenv("CONNECT_URL"))
 	if err != nil {
 		panic(err)
@@ -28,21 +54,11 @@ func main() {
 	sqldb := stdlib.OpenDB(*config)
 	db := bun.NewDB(sqldb, pgdialect.New())
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
 	var v string
 	if err = db.NewSelect().ColumnExpr("version()").Scan(context.Background(), &v); err != nil {
 		log.Fatal(err)
 	}
 	log.Println(v)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	return db
 }
